9a927a7cdd58b22a17b623c0e51876e7bae3f53e
YagoGandara <yago@gandara.com.ar>
2023-06-06 21:50:47 -0300

    funcion graficarTiroVertical funcional y correcta

--- JavaScript/tiroVertical.js
+++ JavaScript/tiroVertical.js
@@ -18,6 +18,7 @@ function graficarTiroVertical() {
     const ctx = canvas.getContext("2d");
 
     // Obtener los valores de los parámetros ingresados por el usuario
+    const fuerza = Number(document.getElementById("velocidad").value);
     const alturaInicial = Number(document.getElementById("altura").value);
     const gravedad = Number(document.getElementById("gravedad").value);
 
@@ -26,15 +27,15 @@ function graficarTiroVertical() {
     const escalaY = 10; // Factor de escala para la posición vertical
 
     // Cálculos
-    const velocidadInicial = Math.sqrt(2 * gravedad * alturaInicial); // Calcular la velocidad inicial
-    const tiempoTotal = (2 * velocidadInicial) / gravedad; // Tiempo total de vuelo
-    const alturaMaxima = alturaInicial + Math.pow(velocidadInicial, 2) / (2 * gravedad); // Altura máxima alcanzada
+    const velocidadInicial = Math.sqrt((2 * fuerza) / gravedad); // Calcular la velocidad inicial
+    const tiempoTotal = (2 * velocidadInicial * Math.sin(Math.PI / 2)) / gravedad; // Tiempo total de vuelo
+    const alturaMaxima = (Math.pow(velocidadInicial, 2) / (2 * gravedad)) + alturaInicial; // Altura máxima alcanzada
 
     // Configuración del lienzo
     const canvasWidth = canvas.width;
     const canvasHeight = canvas.height;
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 2;
     ctx.strokeStyle = "red";
 
     // Dibujar ejes
@@ -58,29 +59,25 @@ function graficarTiroVertical() {
     ctx.moveTo(origenX, origenY - alturaInicial * escalaY);
 
     let t = 0;
-    let x, y;
+    let x = origenX;
+    let y = origenY - alturaInicial * escalaY;
 
-    while (true) {
+    while (y <= origenY) {
         x = origenX + t * escalaX; // Posición horizontal
-        y = origenY - (alturaInicial * escalaY + (velocidadInicial * t - 0.5 * gravedad * Math.pow(t, 2)) * escalaY); // Posición vertical
+        y = origenY - ((velocidadInicial * t * Math.sin(Math.PI / 2)) - 0.5 * gravedad * Math.pow(t, 2) + alturaInicial) * escalaY; // Posición vertical
 
         ctx.lineTo(x, y);
-
-        if (y <= 0) {
-            break; // Terminar el bucle si el objeto toca el suelo (y = 0)
-        }
 
         t += 0.1;
     }
 
     ctx.stroke();
-    mostrarResultados(alturaMaxima, tiempoTotal);
+    mostrarResultados(alturaMaxima, t);
 }
 
 function mostrarResultados(alturaMaxima, tiempo) {
     document.getElementById("distancia").textContent = alturaMaxima.toFixed(2);
     document.getElementById("tiempo").textContent = tiempo.toFixed(2);
 }
-
 
 
