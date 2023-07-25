4cb7ab8d1508b5f747efd8d20fcdab6b9b671600
JuliAA10 <110618330+JuliAA10@users.noreply.github.com>
2023-07-25 11:42:39 -0300

    Ver solo 1er cuadrante en gráficos (corrección)

--- JavaScript/tiroVertical.js
+++ JavaScript/tiroVertical.js
@@ -9,7 +9,7 @@ let correct = (id, valor) => {
         valor = valor.replace(",",".");
     }
     //Creación de variables
-    let altura,altura2, vel, vel2, grav;
+    let altura,altura2, vel, vel2, grav, grav2;
     altura = document.getElementById("altura").value;
     altura2 = document.getElementById("altura");
     vel = document.getElementById("velocidad").value;
@@ -52,8 +52,8 @@ function graficarTiroVertical() {
     const gravedad = Number(document.getElementById("gravedad").value);
 
     // Constantes
-    const escalaX = 10; // Factor de escala para la posición horizontal
-    const escalaY = 10; // Factor de escala para la posición vertical
+    const escala = 10; // Factor de escala para la posición horizontal
+    const intervaloMarcas = 50; // Factor de escala para la posición vertical
 
     // Cálculos
     const velocidadInicial = Math.sqrt((2 * fuerza) / gravedad); // Calcular la velocidad inicial
@@ -64,39 +64,57 @@ function graficarTiroVertical() {
     const canvasWidth = canvas.width;
     const canvasHeight = canvas.height;
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
 
     // Dibujar ejes
     const origenX = 0; // Comienzo del eje x en el extremo izquierdo inferior
     const origenY = canvasHeight; // Comienzo del eje y en el extremo izquierdo inferior
+    const finEjeX = canvasWidth;
+    const finEjeY = 0;
+    ctx.stroke();
 
-    // Dibujar el eje x
+    // Dibujar el eje x con marcas y etiquetas
     ctx.beginPath();
     ctx.strokeStyle = "green";
     ctx.moveTo(origenX, origenY);
-    ctx.lineTo(canvasWidth, origenY);
+    ctx.lineTo(finEjeX, origenY);
     ctx.stroke();
 
-    // Dibujar el eje y
+    for (let posX = intervaloMarcas; posX <= finEjeX; posX += intervaloMarcas) {
+        ctx.beginPath();
+        ctx.moveTo(posX, origenY - 5);
+        ctx.lineTo(posX, origenY + 5);
+        ctx.stroke();
+    }
+
+    // Dibujar el eje y con marcas y etiquetas
     ctx.beginPath();
     ctx.strokeStyle = "green";
     ctx.moveTo(origenX, origenY);
-    ctx.lineTo(origenX, 0);
+    ctx.lineTo(origenX, finEjeY);
     ctx.stroke();
+
+    for (let posY = intervaloMarcas; posY <= origenY; posY += intervaloMarcas) {
+        ctx.beginPath();
+        ctx.moveTo(origenX - 5, posY);
+        ctx.lineTo(origenX + 5, posY);
+        ctx.stroke();
+    }
 
     // Dibujar trayectoria
     ctx.beginPath();
     ctx.strokeStyle = "red";
-    ctx.moveTo(origenX, origenY - alturaInicial * escalaY);
+    ctx.lineWidth = 4;
+    ctx.moveTo(origenX, origenY - alturaInicial * escala);
 
     let t = 0;
     let x = origenX;
-    let y = origenY - alturaInicial * escalaY;
+    let y = origenY - alturaInicial * escala;
 
     while (x <= canvasWidth && y >= 0) {
-        x = origenX + t * escalaX; // Posición horizontal
-        y = origenY - ((velocidadInicial * t * Math.sin(Math.PI / 2)) - 0.5 * gravedad * Math.pow(t, 2) + alturaInicial) * escalaY; // Posición vertical
+        x = origenX + t * escala; // Posición horizontal
+        y = origenY - ((velocidadInicial * t * Math.sin(Math.PI / 2)) - 0.5 * gravedad * Math.pow(t, 2) + alturaInicial) * escala; // Posición vertical
 
         ctx.lineTo(x, y);
 
